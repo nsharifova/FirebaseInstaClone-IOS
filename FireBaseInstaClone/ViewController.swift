79f44d6ebc51116967a0da2bc14a9024c02d5ffa
nsharifova <nurluay00@bk.ru>
2024-10-04 19:48:18 +0400

    Initial Commit

---
+++ FireBaseInstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireBaseInstaClone
+//
+//  Created by Nurluay Sharifova on 04.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
